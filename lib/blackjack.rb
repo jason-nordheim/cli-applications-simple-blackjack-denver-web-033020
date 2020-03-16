863585ff4f5505c11ab9e00538a545b08567ceb4
Jason Nordheim <jason.nordheim@gmail.com>
2020-03-16 14:42:55 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,37 +1,72 @@
-def welcome
-  # code #welcome here
+def welcome()
+  # prints a welcome message to screen
+  puts "Welcome to the Blackjack Table"
 end
 
-def deal_card
-  # code #deal_card here
+def deal_card()
+  # generates a random number between 1-11
+  return rand(1...11)
 end
 
-def display_card_total
-  # code #display_card_total here
+def display_card_total(num)
+  # accepts one argument, the card total
+  # prints the value of the cards to the screen
+  puts "Your cards add up to #{num}"
 end
 
-def prompt_user
-  # code #prompt_user here
+def prompt_user()
+  # gives instructions for hitting or staying
+  puts "Type 'h' to hit or 's' to stay"
 end
 
-def get_user_input
-  # code #get_user_input here
+def get_user_input()
+  # returns the value of a `gets.chomp` method here
+  return gets.chomp()
 end
 
-def end_game
-  # code #end_game here
+def end_game(total)
+  # prints apology, card total, and thank you message
+  puts "Sorry, you hit #{total}. Thanks for playing!"
 end
 
-def initial_round
-  # code #initial_round here
+def initial_round()
+  # calls on #deal_card twice and returns the sum
+  num1 = deal_card
+  num2 = deal_card
+  total = num1 + num2
+  display_card_total(total)
+  return total
 end
 
-def hit?
-  # code hit? here
+def hit?(current_total)
+  # calls on #prompt_user then #get_user_input
+  new_total = current_total
+  prompt_user()
+  user_input = get_user_input
+
+  # user requesting hit
+  if user_input == "h" || user_input == "H"
+    new_card = deal_card()
+    # see if dealing put them over
+    new_total = new_card + current_total
+    if new_total <= 21
+      display_card_total(new_total)
+      hit?()
+    else
+      end_game(new_total)
+    end
+  # user decided to stay
+  elsif user_input == "s" || user_input == "S"
+    display_card_total(current_total)
+  else
+    invalid_command
+  end
+  return new_total
 end
 
-def invalid_command
+def invalid_command()
   # code invalid_command here
+  puts "Please enter a valid command"
 end
 
 #####################################################
@@ -41,4 +76,3 @@ end
 def runner
   # code runner here
 end
-    
